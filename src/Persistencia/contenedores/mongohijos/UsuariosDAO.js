ed6bd889d65ada938a1440158a9fec83c4244a6e
Ivan Sierra <sierraivan93@gmail.com>
2023-04-30 14:16:25 -0300

    Proyecto final backend

---
+++ src/Persistencia/contenedores/mongohijos/UsuariosDAO.js
@@ -0,0 +1,108 @@
+import MongoContainer from "../MongoContainer.js";
+import * as model from "../../models/usuariosModel.js";
+import * as Logger from "../../../scripts/Logger.js";
+import * as Mailer from "../../../Nodemailer.js";
+
+let instance;
+
+export default class UsariosDAO extends MongoContainer {
+  constructor() {
+    super();
+  };
+
+  static getInstance(){
+    if(!instance){
+      instance = new UsariosDAO();
+    };
+    return instance;
+  };
+
+  //GET
+  async getAll() {
+    try {
+      await this.connect();
+      let res = await model.usuarios.find({}, { __v: 0 });
+      await this.disconnect();
+      return res;
+    } catch (error) {
+      Logger.logError.error(error);
+    };
+  };
+
+  async getById(id) {
+    try {
+      await this.connect();
+      let res = await model.usuarios.find({ _id: id }, { __v: 0 });
+      await this.disconnect();
+      return res;
+    } catch (error) {
+      Logger.logError.error(error);
+    };
+  };
+
+  async getByEmail(email) {
+    try {
+      await this.connect();
+      let res = await model.usuarios.findOne({ email: email }, { __v: 0 });
+      await this.disconnect();
+      return res;
+    } catch (error) {
+      Logger.logError.error(error);
+    };
+  };
+
+  //POST
+  async save(user) {
+    try {
+      await this.connect();
+      const nuevoUser = await model.usuarios.create(user);
+      // enviar mail al admin registro nuevo
+      const mailOptions = {
+        from: "App Node",
+        to: Mailer.admEmail,
+        subject: "Nuevo registro",
+        html: `${nuevoUser}`,
+      };
+      Mailer.ecommerceGmail.sendMail(mailOptions);
+      await this.disconnect();
+      return nuevoUser;
+    } catch (error) {
+      Logger.logError.error(error);
+    };
+  };
+
+  //PUT
+  async put(userId, user) {
+    try {
+      await this.connect();
+      let res = await model.usuarios.updateOne({ _id: userId }, user);
+      await this.disconnect();
+      return res;
+    } catch (error) {
+      Logger.logError.error(error);
+    };
+  };
+
+  //DELETE
+  async deleteById(userId) {
+    try {
+      await this.connect();
+      let res = await model.usuarios.deleteOne({ _id: userId });
+      await this.disconnect();
+      return res;
+    } catch (error) {
+      Logger.logError.error(error);
+    };
+  };
+
+  async deleteAll() {
+    try {
+      await this.connect();
+      let res = await model.usuarios.deleteMany();
+      await this.disconnect();
+      return res;
+    } catch (error) {
+      Logger.logError.error(error);
+    };
+  };
+};
---
+++ src/Service/Service.js
@@ -0,0 +1,74 @@
+import ProductosDAOdb from "../Persistencia/contenedores/mongohijos/ProductosDAOdb.js";
+import MensajesDAODb from "../Persistencia/contenedores/mongohijos/MensajesDAOdb.js";
+import UsuariosDAO from "../Persistencia/contenedores/mongohijos/UsuariosDAO.js";
+import CarritosDAO from "../Persistencia/contenedores/mongohijos/CarritosDAO.js";
+
+const Users = UsuariosDAO.getInstance();
+const Messages = MensajesDAODb.getInstance();
+const Products = ProductosDAOdb.getInstance();
+const Carros = CarritosDAO.getInstance();
+
+
+async function getAllProducts(){
+    return await Products.getAll();
+};
+
+async function getProdById(id){
+    return await Products.getById(id);
+}
+
+async function getProdByTags(tags){
+    return await Products.getByTags(tags);
+};
+
+async function saveManyProducts(prods){
+    return await Products.saveMany(prods);
+};
+
+async function deleteAllProds(){
+    await Products.deleteAll();
+    return;
+};
+
+async function createCart(owner){
+    return await Carros.crearCarrito(owner);
+};
+
+async function addProdToCart(cartId, prodId){
+    return await Carros.añadirProducto(cartId,prodId);
+};
+
+async function getCartByOwner(owner){
+    return await Carros.getCarritoIdByDueño(owner);
+};
+
+async function getCartById(id){
+    return await Carros.getById(id);
+};
+
+async function deleteProdFromCart(cartId, prodId){
+    return await Carros.eliminarProducto(cartId, prodId);
+};
+
+async function getUserByEmail(email){
+    return await Users.getByEmail(email);
+};
+
+async function saveUser(user){
+    return await Users.save(user);
+};
+
+export default {
+    getAllProducts,
+    getProdById,
+    getProdByTags,
+    saveManyProducts,
+    deleteAllProds,
+    createCart,
+    addProdToCart,
+    getCartByOwner,
+    getCartById,
+    deleteProdFromCart,
+    getUserByEmail,
+    saveUser
+};
